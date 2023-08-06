ef63a1d329f730422bb58b7f707a942d68af1b34
AberonAmarite <abay.darmenov@gmail.com>
2023-08-06 18:14:56 +0600

    postgre changes

--- mars_service/mars_service/settings.py
+++ mars_service/mars_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*^h@-d5w_yne*=mhskmbj=#8krlqcndexd7sab_ht44e4)1o&!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['164.90.138.16']
 
 
 # Application definition
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'mars_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'NAME': 'covid_db',
+        'USER': 'abay',
+        'PASSWORD': 'abay2023',
         'HOST': 'localhost',
         'PORT': '5432'
     }
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
